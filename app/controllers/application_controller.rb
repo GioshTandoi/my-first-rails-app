c467e3de0a00449761b6e9ed95d31af91fbfce02
Giorgia Tandoi <mariagio.tandoi@gmail.com>
2022-02-14 09:48:51 +0100

    tutorials steps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # Before you run any actions call set current user.
+  # That is what will happen before you make a request
+  # in your rails app.
+  before_action :set_current_user
+
+  def set_current_user
+    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # GET /about
-  get "about-us", to: "about#index", as: :about
-  get "sign-up", to: "registration#new"
-  post "sign-up", to:"registration#create"
-  root to: "main#index"
+  get 'about-us', to: 'about#index', as: :about
+
+  get 'sign-up', to: 'registration#new'
+  post 'sign-up', to: 'registration#create'
+
+  get 'sign-in', to: 'sessions#new'
+  post 'sign-in', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
+
+  root to: 'main#index'
 end
